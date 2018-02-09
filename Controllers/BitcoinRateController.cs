166aa6796b22f0d1c81c42b79e49b8ded155ae63
SergeyDushkin <leirbythe@gmail.com>
2018-02-09 20:07:09 +0300

    merge asp & site

---
+++ Controllers/BitcoinRateController.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace blockchain.rate.service.Controllers
+{
+    [Route("rate/bitcoin")]
+    public class BitcoinRateController : Controller
+    {
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            using(var client = new HttpClient())
+            {
+                var result = await client.GetAsync($"https://api.bitfinex.com/v1/ticker/btcusd"); 
+    
+                var stream = await result.Content.ReadAsStringAsync(); 
+
+                var rate = JObject.Parse(stream).Property("last_price").Value.ToString();
+    
+                return Ok(rate);
+            }
+        }
+    }
+}
---
+++ Controllers/CurrencyRateController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System.Xml.Linq;
+
+namespace blockchain.rate.service.Controllers
+{
+    [Route("rate/usd")]
+    public class CurrencyRateController : Controller
+    {
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            using (var client = new HttpClient())
+            {
+                Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+                var result = await client.GetAsync($"http://www.moex.com/export/derivatives/currency-rate.aspx?language=ru&currency=USD/RUB&moment_start={DateTime.Today.AddDays(-1).ToString("yyyy-MM-dd")}&moment_end={DateTime.Today.ToString("yyyy-MM-dd")}"); 
+    
+                var stream = await result.Content.ReadAsStringAsync(); 
+
+                var correctDocument = XDocument.Parse(stream);
+
+                var rate = correctDocument.Descendants("rate").FirstOrDefault().Attribute("value").Value;
+
+                return Ok(rate);
+            }
+        }   
+    }
+    
+}
