fa96593426e7577d28e6b4edcc297813eb789a2b
SergeyDushkin <leirbythe@gmail.com>
2018-02-27 15:45:39 +0300

    add mailgun reference

--- Controllers/InvoiceController.cs
+++ Controllers/InvoiceController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Serialization;
-using MailChimp.Net;
-using MailChimp.Net.Interfaces;
+using FluentEmail.Core;
+using FluentEmail.Core.Models;
+using FluentEmail.Mailgun;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 
@@ -34,8 +36,48 @@ namespace blockchain.rate.service.Controllers
                 xs.Serialize(xml, invoice);
             }
 
-            //IMailChimpManager manager = new MailChimpManager("f235cd845b6d50846d5ff7c7adb001d7-us17");
+
+            var body = @"
+Здравствуйте Сергей
+
+<a href='https://cryptoX.space/OLjrzcMbn88FXulGfhrOlucTIvXB76GbU38'>Заявка #%OrderNum% от %OrderDateTime% - %Status%</a>
+Заявка #123 от 26.02.18 14:30 - Ожидает перевода
+
+Покупка %ToAmount% %ToCur%
+Покупка 0.085 BTC
+
+1 %FromCur% = %Rate% %ToCur%
+1 RUR = 0.00008893 BTC
+
+Адрес отправителя: %Phone vs CryptoAddress%
+Адрес отправителя: +7 925 878 8681
+
+Адрес получателя: %Phone vs CryptoAddress%
+Адрес получателя: 17XSs9yq5M9cRDU4XGtz45UTEcqMEdm6RF";
+
+            var sender = new MailgunSender(
+                "sandbox2de279b82e6f443bbcc3e6cea7ebca3b.mailgun.org", // Mailgun Domain
+                "key-f942a01bbc9473142f4c8dace3c34f78" // Mailgun API Key
+            );
+
+            var to = new List<Address>() 
+            { 
+                new Address("leirbythe@gmail.com"), 
+                new Address("rodionov.sergey.v@gmail.com") 
+            };
+
+            var subject = "cryptoX.space: Заявка #124 - Ожидает перевода";
+
+            Email.DefaultSender = sender;
             
+            var email = Email
+                .From("leirbythe@gmail.com")
+                .To(to)
+                .Subject(subject)
+                .Body(body);
+
+            var response = await email.SendAsync();
+
             return Ok(invoice);
         }
     }
